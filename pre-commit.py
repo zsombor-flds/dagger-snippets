4456d16dca3755535c8fd017e4d911101f429320
zsombor-flds <zsombor.foldesi@hiflylabs.com>
2023-01-07 20:19:30 +0100

    add pre-commit

---
+++ pre-commit.py
@@ -0,0 +1,30 @@
+import sys
+import anyio
+import dagger
+
+
+async def test():
+    """
+    Run sqlfmt on a given dbt project
+    """
+    async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
+        src = client.host().directory(".")
+
+        python = (
+            client.container()
+            .from_("python:3.10-slim-buster")
+            .with_mounted_directory("/demo", src)
+            .with_workdir("/demo/")
+            .with_exec(["apt", "update", "-y"])
+            .with_exec(["apt", "install", "-y", "git"])
+            .with_exec(["pip", "install", "pre-commit"])
+            .with_exec(["pre-commit", "install"])
+            .with_exec(["pre-commit", "run", "--all-files"])
+        )
+        # execute
+        await python.stdout()
+        # await python.exit_code()
+
+
+if __name__ == "__main__":
+    anyio.run(test)
